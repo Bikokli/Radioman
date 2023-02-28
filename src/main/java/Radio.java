b21a1bd7548d7606fccd06da364869f2a1206e53
Vladimir Svet <Bikokli9@gmail.com>
2023-03-01 02:42:18 +0300

    radio

---
+++ src/main/java/Radio.java
@@ -0,0 +1,45 @@
+public class Radio {
+    private int volume = 0;
+    private int radioStations = 0;
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public int getRadioStations() {
+        return radioStations;
+    }
+
+    public void increaseVolume() {
+        if (volume < 10) {
+            volume++;
+        }
+        System.out.println("volume = " + volume);
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+        System.out.println("volume = " + volume);
+    }
+
+    public int nextRadioStation() {
+        if (radioStations < 9) {
+            radioStations++;
+        } else {
+            radioStations = 0;
+        }
+        System.out.println("radio station is " + radioStations);
+        return 0;
+    }
+
+    public void prevRadioStation() {
+        if (radioStations > 0) {
+            radioStations--;
+        } else {
+            radioStations = 9;
+        }
+        System.out.println("radio station is " + radioStations);
+    }
+}
---
+++ src/test/java/RadioTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    @Test
+    public void shouldSetMaxVolume() {
+        Radio radio = new Radio();
+        for (int i = 0; i < 10; i++) {
+            radio.increaseVolume();
+        }
+        int expected = 10;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMinVolume() {
+        Radio radio = new Radio();
+        radio.increaseVolume();
+        for (int i = 0; i < 10; i++) {
+            radio.decreaseVolume();
+        }
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMaxRadioStation() {
+        Radio radio = new Radio();
+        radio.prevRadioStation();
+        for (int i = 0; i < 9; i++) {
+            radio.nextRadioStation();
+        }
+        int expected = 8;
+        int actual = radio.getRadioStations();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetMinRadioStation() {
+        Radio radio = new Radio();
+        radio.nextRadioStation();
+        for (int i = 0; i < 10; i++) {
+            radio.prevRadioStation();
+        }
+        int expected = 1;
+        int actual = radio.getRadioStations();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
